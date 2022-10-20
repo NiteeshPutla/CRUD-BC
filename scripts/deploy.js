3639a5fc61452065fffcefeb74dc391052048d37
nitesh <niteeshjoyal@gmail.com>
2022-10-20 16:12:36 +0530

    CRUD in solidity

---
+++ scripts/deploy.js
@@ -0,0 +1,49 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  const lockedAmount = hre.ethers.utils.parseEther("1");
+
+  const CRUD = await hre.ethers.getContractFactory("CRUD");
+  const crud = await CRUD.deploy();
+
+  await crud.deployed();
+
+  console.log(
+    `Crud deployed to:`,crud.address
+  );
+  const totalEmployees1=await crud.totalEmployees();
+
+  const response=await crud.create("Niteesh","niteeshjoyal@gmail.com","24","0x5FbDB2315678afecb367f032d93F642f64180aa3")
+  const response1=await crud.create("jessy","niteeshjessy@gmail.com","24","0x5B38Da6a701c568545dCfcB03FcB875f56beddC4")
+
+  const totalEmployees2=await crud.totalEmployees();
+  const employee=await crud.employees(0);
+   console.log(employee);
+   console.log(totalEmployees1,totalEmployees2);
+   console.log(response);
+   await crud.updateEmployee("Joy","niteeshjoyal@gmail.com")
+   const employee2=await crud.employees(0);
+    console.log(employee2);
+
+   await crud.deleteEmployee("niteeshjoyal@gmail.com")
+   const employeeDelete=await crud.employees(0);
+   console.log(employeeDelete);
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
