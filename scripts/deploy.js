0a01467ac0cdf58aa1db58f7c1e44109775995aa
nitesh <niteeshjoyal@gmail.com>
2022-10-25 11:37:52 +0530

    Merge branch 'master' of https://github.com/NiteeshPutla/CRUD-BC

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,11 +7,7 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
 
   const CRUD = await hre.ethers.getContractFactory("CRUD");
   const crud = await CRUD.deploy();
